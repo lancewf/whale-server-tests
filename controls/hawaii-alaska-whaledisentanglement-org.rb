2925e198d2a9a9caa1acccfbf92bd628e608bdeb
Lance Finfrock <lfinfrock@chef.io>
2020-04-25 10:43:16 -0700

    Wordpress site are timing out
    Increased the read time for all the wordpress sites because they were
    timing out.
    
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- controls/hawaii-alaska-whaledisentanglement-org.rb
+++ controls/hawaii-alaska-whaledisentanglement-org.rb
@@ -5,14 +5,14 @@ control 'hawaii-alaska-whaledisentanglement-org' do
   title 'Hawaii Alaska Whale Disentanglement site is verified'
   desc 'test that the hawaii-alaska.whaledisentanglement.org is working'
 
-  describe inspec.http("https://hawaii-alaska.whaledisentanglement.org", max_redirects: 1) do
+  describe inspec.http("https://hawaii-alaska.whaledisentanglement.org", max_redirects: 1, read_timeout: 90) do
     its('status') { should eq(200) }
     its('body') { 
       should match('<h2 class="site-description">Hawaii and Alaska</h2>')
       should_not match('cdn.eeduelements.com')
     }
   end
-  describe inspec.http("http://hawaii-alaska.whaledisentanglement.org", max_redirects: 2) do
+  describe inspec.http("http://hawaii-alaska.whaledisentanglement.org", max_redirects: 2, read_timeout: 90) do
     its('status') { should eq(200) }
     its('body') { should match('<h2 class="site-description">Hawaii and Alaska</h2>') } 
   end
--- controls/media-whaledisentanglement-org.rb
+++ controls/media-whaledisentanglement-org.rb
@@ -5,14 +5,14 @@ control 'media-whaledisentanglement-org' do
   title 'Media Whale Disentanglement site is verified'
   desc 'test that the media.whaledisentanglement.org is working'
 
-  describe inspec.http("https://media.whaledisentanglement.org", max_redirects: 1) do
+  describe inspec.http("https://media.whaledisentanglement.org", max_redirects: 1, read_timeout: 90) do
     its('status') { should eq(200) }
     its('body') { 
       should match('<h2 class="site-description">Media</h2>')
       should_not match('cdn.eeduelements.com')
     } 
   end
-  describe inspec.http("http://media.whaledisentanglement.org", max_redirects: 2) do
+  describe inspec.http("http://media.whaledisentanglement.org", max_redirects: 2, read_timeout: 90) do
     its('status') { should eq(200) }
     its('body') { should match('<h2 class="site-description">Media</h2>') } 
   end
